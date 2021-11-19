3588a3faaafc57604042abc98a4c74cb1ed791fd
corinmanning2315 <manninc2@mail.uc.edu>
2021-11-18 21:17:39 -0500

    functions and buttons

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,10 @@
+gameContainer.classList.add("d-none");
+newGameButton.classList.add("d-none");
+
 const familyOne = document.getElementById("familyOne");
 const familyTwo = document.getElementById("familyTwo");
 
 const startButton = document.getElementById("startButton");
-
-gameContainer.classList.add("d-none");
 
 const familyOneScore = document.getElementById("familyOneScore");
 const familyTwoScore = document.getElementById("familyTwoScore");
@@ -25,11 +26,10 @@ const question = document.getElementById("question");
 const userGuess = document.getElementById("userGuess");
 
 const submitButton = document.getElementById("submitButton");
-newGameButton.classList.add("d-none");
 
-const strike1 = document.getElementById("strike1");
-const strike2 = document.getElementById("strike2");
-const strike3 = document.getElementById("strike3");
+const strikeOne = document.getElementById("strikeOne");
+const strikeTwo = document.getElementById("strikeTwo");
+const strikeThree = document.getElementById("strikeThree");
 
 
 
@@ -160,4 +160,142 @@ function setPlaceholders() {
         scoreholder.value = parseInt(scoreholder.value) + questions[questionSelector].answerEightPts;
             
     }
+    else{
+        addStrike();
+    }
+
+    function clearStrikes() {
+        strikeOne.value = "";
+        strikeTwo.value = "";
+        strikeThree.value = "";
+    }
+
+    function addStrike() {
+        if (strikeOne.value == "") {
+            strikeOne.value = "X";
+        }
+        else if (strikeTwo.value == "") {
+            strikeTwo.value = "X"
+        }
+        else if (strikeThree.value == "") {
+            strikeThree.value = "X"
+            if (teamTurn == 1) {
+                teamTurn = 2;
+            }
+            else if (teamTurn == 2) {
+                teamTurn = 1;
+            }
+        }
+        else {
+            submitButton.classList.add("d-none");
+            newGameButton.classList.remove("d-none");
+            if (teamTurn == 1) {
+                scoreholderOne.value = parseInt(scoreholder.value) + parseInt(scoreholderOne.value);
+            }
+            else if (teamTurn == 2) {
+                scoreholderTwo.value = parseInt(scoreholder.value) + parseInt(scoreholderTwo.value);
+            }
+            if (questions[questionSelector].numberOfQuestions == 1) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 2) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+        
+            }
+            else if (questions[questionSelector].numberOfQuestions == 3) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 4) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+                answerFour.placeholder = questions[questionSelector].answerFour;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 5) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+                answerFour.placeholder = questions[questionSelector].answerFour;
+                answerFive.placeholder = questions[questionSelector].answerFive;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 6) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+                answerFour.placeholder = questions[questionSelector].answerFour;
+                answerFive.placeholder = questions[questionSelector].answerFive;
+                answerSix.placeholder = questions[questionSelector].answerSix;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 7) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+                answerFour.placeholder = questions[questionSelector].answerFour;
+                answerFive.placeholder = questions[questionSelector].answerFive;
+                answerSix.placeholder = questions[questionSelector].answerSix;
+                answerSeven.placeholder = questions[questionSelector].answerSeven;
+            }
+            else if (questions[questionSelector].numberOfQuestions == 8) {
+                answerOne.placeholder = questions[questionSelector].answerOne;
+                answerTwo.placeholder = questions[questionSelector].answerTwo;
+                answerThree.placeholder = questions[questionSelector].answerThree;
+                answerFour.placeholder = questions[questionSelector].answerFour;
+                answerFive.placeholder = questions[questionSelector].answerFive;
+                answerSix.placeholder = questions[questionSelector].answerSix;
+                answerSeven.placeholder = questions[questionSelector].answerSeven;
+                answerEight.placeholder = questions[questionSelector].answerEight;
+            }
+        }
+    };
+
+     startButton.addEventListener('click', function (event) {
+        gameContainer.classList.remove("d-none");
+        startContainer.classList.add("d-none");
+        familyOneScore.innerHTML = familyOne.value;
+        familyTwoScore.innerHTML = familyTwo.value;
+        selectQuestion();
+        setPlaceholders();
+        question.innerHTML = questions[questionSelector].question;
+     });
+
+     submitButton.addEventListener('click', function (event) {
+        guessChecker();
+        userGuess.value = "";
+
+        if (correctCounter == questions[questionSelector].numberOfQuestions) {
+           submitButton.classList.add("d-none");
+           newGameButton.classList.remove("d-none");
+           if (teamTurn == 1) {
+              scoreholderOne.value = parseInt(scoreholder.value) + parseInt(scoreholderOne.value);
+              teamTurn = 2;
+           }
+           else if (teamTurn == 2) {
+              scoreholderTwo.value = parseInt(scoreholder.value) + parseInt(scoreholderTwo.value);
+              teamTurn = 1;
+           }
+        }
+     
+        console.log(correctCounter);
+     });
+
+     newGameButton.addEventListener('click', function (event) {
+        selectQuestion();
+        clearPlaceholders();
+        correctCounter = 0;
+        question.innerHTML = questions[questionSelector].question;
+        clearStrikes();
+        setPlaceholders();
+        newGameButton.classList.add("d-none");
+        submitButton.classList.remove("d-none");
+        roundCounter += 1;
+        if (roundCounter % 2 == 0) {
+           teamTurn = 2;
+        }
+        else {
+           teamTurn = 1;
+        }
+     });
  }
